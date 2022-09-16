8161909989ada239fb66244694ee3f16e4bd80d2
unknown <chithrakalaiselvan75@gmail.com>
2022-09-16 13:44:18 -0400

    new feature

--- src/main/java/com/example/demo/Welcome.java
+++ src/main/java/com/example/demo/Welcome.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class Welcome {
     public String display()
     {
         return "Hello World";
+    }
+    @GetMapping("Id")
+    public int output()
+    {
+        return 55;
     }
 }
